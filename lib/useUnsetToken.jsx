6b7ba486097cd2c13f2c4743e27302f39792e125
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-03 17:37:30 +0100

    Changed names

--- lib/useUnsetToken.jsx
+++ lib/useUnsetToken.jsx
@@ -2,7 +2,6 @@ import { useCookies } from "react-cookie";
 
 const useUnsetToken = () => {
     const [_, removeCookie] = useCookies(["jwt", "username", "id"]);
-    console.log("unsetting token");
     const unsetToken = () => {
         removeCookie("jwt", { path: "/" });
         removeCookie("username", { path: "/" });
