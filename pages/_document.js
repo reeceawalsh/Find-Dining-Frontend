713f357acfb578f9c33ceee4c38bbb24cdf8c463
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-03 19:46:04 +0100

    Fixed favicon location

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="shortcut icon" href="../static/favicon.ico" />
+                    <link rel="shortcut icon" href="../public/favicon.ico" />
                 </Head>
                 <body>
                     <Main />
