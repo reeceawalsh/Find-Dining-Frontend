81c4ea561573c7d42402a64613250ef7a28b257d
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-03 19:06:01 +0100

    updated validate email

--- fieldValidation/Email.jsx
+++ fieldValidation/Email.jsx
@@ -1,12 +1,7 @@
-// email validation, takes an email and errors and returns the new errors.
 export default function validateEmail(errors, email) {
     if (!email) {
         errors.email = "Email is required.";
-    } else if (
-        !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g.test(
-            email
-        )
-    ) {
+    } else if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/g.test(email)) {
         errors.email = "Must be a valid email address.";
     }
 
