ebcfab6627c052016676542b22b4e3aa1b5765c6
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-03 19:01:46 +0100

    Added a favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head></Head>
+                <Head>
+                    <link rel="shortcut icon" href="/static/favicon.ico" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
