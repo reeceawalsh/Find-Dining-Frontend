e449cc08b8fe92fd9e7c79e9509af39279ef023f
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-06-28 20:51:28 +0100

    Updated user function to use an admin token as the jwt is saying forbidden

--- pages/api/user.js
+++ pages/api/user.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
                 },
                 {
                     headers: {
-                        Authorization: `Bearer ${token}`,
+                        Authorization: `Bearer ${process.env.ADMIN_TOKEN}`,
                     },
                 }
             );
